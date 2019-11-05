3219ce51563a4dad714c82687a2d1e25b2d668d9
dantemessy <thepunishar1@gmail.com>
2019-11-05 22:47:11 +0200

    Dynamic

---
+++ js/response.js
@@ -0,0 +1,11 @@
+var answer = prompt("Do You Watch Anime (if Yes enter 1 if No enter 2)")
+var response ;
+
+if (answer == 1 ){
+    response = "You Will Love This Site!"
+} else {
+    response = "Are You Lost ??"
+}
+
+
+document.write('<h2>' +response+ '<h2>');
