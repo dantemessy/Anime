d8b8f9465e2c59be3f15bc37ccf39cc89e784015
dantemessy <thepunishar1@gmail.com>
2019-11-06 14:07:44 +0200

    function

--- js/hello.js
+++ js/hello.js
@@ -3,15 +3,30 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
+function showGreeting (){
 
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!'; 
-} else {
-    greeting = 'Welcome!';
+    if (hourNow > 18) {
+        greeting = 'Good evening!';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!'; 
+    } else {
+        greeting = 'Welcome!';
+    }
+return greeting;
 }
+showGreeting();
 
-document.write('<h3>' +greeting+ '</h3>'); 
+
+// if (hourNow > 18) {
+//     greeting = 'Good evening!';
+// } else if (hourNow > 12) {
+//     greeting = 'Good afternoon!';
+// } else if (hourNow > 0) {
+//     greeting = 'Good morning!'; 
+// } else {
+//     greeting = 'Welcome!';
+// }
+
+// document.write('<h3>' +showGreeting+ '</h3>'); 
--- js/response.js
+++ js/response.js
@@ -1,11 +1,22 @@
+'use strict'
 var answer = prompt("Do You Watch Anime (if Yes enter 1 if No enter 2)")
 var response ;
+var baka ;
 
-if (answer == 1 ){
-    response = "You Will Love This Site!"
-} else {
-    response = "Are You Lost ??"
-}
+function responseImge(){
+    if (answer == 1 ){
+        response = "You Will Love This Site!"
+    } else {
+        response = "If You Dont Watch it, What are doing Here ??"
+        // baka = '<img src="https://www.sccpre.cat/mypng/full/247-2478192_png-baka-anime-emoji-for-discord.png" />';  
+    }
+    return response;
+} responseImge();
 
+// if (answer == 1 ){
+//     response = "You Will Love This Site!"  
+// } else {
+//     response = "If You Dont Watch it, What are doing Here ??"   
+// }
 
-document.write('<h2>' +response+ '<h2>');
+// document.write('<h2>' +response+ '<h2>');
